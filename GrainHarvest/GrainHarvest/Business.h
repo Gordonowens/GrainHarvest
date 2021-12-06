3d301ef7f69790481e9a467c0c83ecf3b9f309c8
Gordonowens <gordon.owens7@gmail.com>
2021-12-06 16:25:37 +1100

    update knapsack function to return struct

--- GrainHarvest/GrainHarvest/Business.h
+++ GrainHarvest/GrainHarvest/Business.h
@@ -11,6 +11,11 @@ using namespace std;
 //Represent the grain silo company
 //runs the logic of the grain business
 //holds data relevant to running of grain business
+
+struct solution {
+    int number_elements;
+    int* solution;
+};
 class Business {
 public:
 
@@ -21,6 +26,8 @@ public:
 
     string grain_types[3] = { "A", "B", "C" };
     Order *orders;
+
+    solution schedule_solution;
 
     Business(int y, Order* new_orders, int number_orders) {
 
@@ -73,6 +80,9 @@ public:
         int* times = new int[number_order];
         int* values = new int[number_order];
 
+        int* selection = new int[3];
+        struct solution schedule_solution;
+
         for (int i = 0; i < number_order; i++) {
             times[i] = orders[i].GetOrderTime();
         }
@@ -82,7 +92,13 @@ public:
         }
 
         //pass order values and times to knapsac algorithm
-        knapsackDyProg(times, values, max_time, number_order);
+        schedule_solution = knapsackDyProg(times, values, max_time, number_order);
+
+        for (int i = 0; i < schedule_solution.number_elements; i++) {
+
+            cout << " " + to_string(schedule_solution.solution[i]);
+        }
+        
     }
 
     void PrintOrders() {
@@ -128,9 +144,26 @@ public:
 
     }
 
-    void knapsackDyProg(int* W, int* V, int M, int n) {
+    int* resize(int* arr, int number_elements)
+    {
+        int* resize_arr = new int[number_elements + 1];
+        for (int i = 0; i < number_elements; i++) {
+            resize_arr[i] = arr[i];
+        }
+
+        return resize_arr;
+    }
+
+    struct solution knapsackDyProg(int* W, int* V, int M, int n) {
 
         int** B = new int* [n + 1];
+
+        int selection_number = 1;
+        int* selection_array = new int[selection_number];
+        selection_array[0] = 0;
+
+        struct solution schedule;
+        
 
         for (int i = 0; i < (n + 1); i++) {
             B[i] = new int[M + 1];
@@ -150,9 +183,9 @@ public:
                     B[i][j] = B[i - 1][j - W[i - 1]] + V[i - 1];
                 }
 
-                cout << to_string(B[i][j]) << " ";
+                //cout << to_string(B[i][j]) << " ";
             }
-            cout << "\n";
+            //cout << "\n";
         }
 
         cout << "Max Value:\t" << to_string(B[n][M]);
@@ -162,11 +195,23 @@ public:
         while (n != 0) {
             if (B[n][M] != B[n - 1][M]) {
                 cout << "\tPackage " << to_string(n) << " with W = " + to_string(W[n - 1]) << " and Value = " + to_string(V[n - 1]);
-
+                if (selection_array[0] != 0) {
+                    selection_array = resize(selection_array, selection_number);
+                    selection_array[selection_number] = n;
+                    selection_number++;
+                }
+                else {
+                    selection_array[selection_number - 1] = n;
+                    
+                }
                 M = M - W[n - 1];
             }
 
             n--;
         }
+
+        schedule.number_elements = selection_number;
+        schedule.solution = selection_array;
+        return schedule;
     }
 };
